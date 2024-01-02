2a162704ac1edb912616b5fc6f2034b0c19c3394
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-02 10:00:00 +0300

    Added basic templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Home - Main Page',
+        'content': "Furniture Store HOME",
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Home - About Us',
+        'content': "About Us",
+        'text_on_page': "Text about why this store is awesome and how great our products are.",
+    }
+
+    return render(request, 'main/about.html', context)
