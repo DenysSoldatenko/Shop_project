47db3157072e83a8568e36110513ad7e7c844e8e
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-10 10:00:00 +0300

    Added the Category class

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.name
