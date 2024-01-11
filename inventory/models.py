ee1d2f69ced9013d9be7ba803790e1167dd67fc2
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-11 10:00:00 +0300

    Added the Product class

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -13,3 +14,34 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+    image = models.ImageField(upload_to='inventory_images', blank=True, null=True, verbose_name='Image')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Price')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Discount (%)')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Quantity')
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, verbose_name='Category')
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ("id",)
+
+    def __str__(self):
+        return f'{self.name} Quantity - {self.quantity}'
+
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+
+    def display_id(self):
+        return f"{self.id:05}"
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+        return self.price
