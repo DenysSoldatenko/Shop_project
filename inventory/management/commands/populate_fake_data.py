ccb86525b02c43273fabc5fb7336c76ad6503c5f
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-14 10:00:00 +0300

    Added a check for uniqueness for categories and products

--- inventory/management/commands/populate_fake_data.py
+++ inventory/management/commands/populate_fake_data.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 from faker import Faker
+
 from inventory.models import Category, Product
+
 
 class Command(BaseCommand):
     help = "Generate fake data for Category and Product models"
@@ -12,12 +15,19 @@ class Command(BaseCommand):
         for _ in range(10):
             category_name = fake.word().capitalize()
             category_slug = fake.slug()
-            category = Category.objects.create(
-                name=category_name,
-                slug=category_slug
-            )
-            categories.append(category)
-            self.stdout.write(self.style.SUCCESS(f'Successfully created category: {category.name}'))
+
+            try:
+                category = Category.objects.create(
+                    name=category_name,
+                    slug=category_slug
+                )
+                categories.append(category)
+                self.stdout.write(self.style.SUCCESS(f'Successfully created category: {category.name}'))
+
+            except IntegrityError as e:
+                self.stdout.write(self.style.WARNING(
+                    f'Category name or slug "{category_name}" already exists. Skipping this category.'))
+                continue
 
         for _ in range(20):
             product_name = fake.word().capitalize()
@@ -29,15 +39,20 @@ class Command(BaseCommand):
             product_quantity = fake.random_int(min=1, max=100)
             product_category = fake.random_element(categories)
 
-            product = Product.objects.create(
-                name=product_name,
-                slug=product_slug,
-                description=product_description,
-                image=product_image,
-                price=product_price,
-                discount=product_discount,
-                quantity=product_quantity,
-                category=product_category
-            )
+            try:
+                product = Product.objects.create(
+                    name=product_name,
+                    slug=product_slug,
+                    description=product_description,
+                    image=product_image,
+                    price=product_price,
+                    discount=product_discount,
+                    quantity=product_quantity,
+                    category=product_category
+                )
+                self.stdout.write(self.style.SUCCESS(f'Successfully created product: {product.name}'))
 
-            self.stdout.write(self.style.SUCCESS(f'Successfully created product: {product.name}'))
+            except IntegrityError as e:
+                self.stdout.write(
+                    self.style.WARNING(f'Product name "{product_name}" already exists. Skipping this product.'))
+                continue
