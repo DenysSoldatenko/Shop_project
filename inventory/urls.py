0c15b3c157eabbdf01479e75844d642304de119c
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-25 10:00:00 +0300

    Added search functionality to filter products by ID

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,6 +5,7 @@ from inventory import views
 app_name = 'inventory'
 
 urlpatterns = [
+    path('search/', views.product_list, name='search'),
     path('<slug:category_slug>/', views.product_list, name='product_list'),
     path('product/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -2,19 +2,28 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from inventory.models import Product
+from inventory.utils import q_search
 
-def product_list(request, category_slug):
+
+def product_list(request, category_slug=None):
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', 'default')
+    query = request.GET.get('query', None)
 
-    products = Product.objects.filter(category__slug=category_slug) if category_slug != 'all' else Product.objects.all()
+    if category_slug == "all":
+        products = Product.objects.all()
+    elif query:
+        products = q_search(query)
+    else:
+        products = Product.objects.filter(category__slug=category_slug)
+
     if on_sale:
         products = products.filter(discount__gt=0)
     if order_by != 'default':
         products = products.order_by(order_by)
 
-    paginator = Paginator(products, 9)
+    paginator = Paginator(products, 6)
     current_page = paginator.get_page(page)
 
     context = {
