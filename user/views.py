b96db82230e35011642001fdae25b1d2ea16802e
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-30 10:00:00 +0300

    Added templates for the 'user' section

--- user/views.py
+++ user/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    context = {
+        'title': 'Login'
+    }
+    return render(request, 'user/login.html', context)
+
+def register(request):
+    context = {
+        'title': 'Register'
+    }
+    return render(request, 'user/register.html', context)
+
+def profile(request):
+    context = {
+        'title': 'Profile'
+    }
+    return render(request, 'user/profile.html', context)
+
+def logout(request):
+    pass
