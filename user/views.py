10b0e442b198192cb4a23abeda6c7bd0eadc1262
DenysSoldatenko <soldatenko911@gmail.com>
2024-02-01 10:00:00 +0300

    Added login functionality

--- user/views.py
+++ user/views.py
@@ -1,10 +1,31 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from user.forms import UserLoginForm
+
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('core:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Login'
+        'title': 'Login',
+        'form': form
     }
+
     return render(request, 'user/login.html', context)
+
 
 def register(request):
     context = {
@@ -12,11 +33,13 @@ def register(request):
     }
     return render(request, 'user/register.html', context)
 
+
 def profile(request):
     context = {
         'title': 'Profile'
     }
     return render(request, 'user/profile.html', context)
+
 
 def logout(request):
     pass
