41a5ee863de3889eb87d7a10306a0ac1c0785780
DenysSoldatenko <soldatenko911@gmail.com>
2024-02-16 10:00:00 +0300

    Refactored the admin panel for cart management

--- cart/admin.py
+++ cart/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 from cart.models import Cart
 
 
-admin.site.register(Cart)
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["user_display", "product_display", "quantity", "created_timestamp", ]
+    list_filter = ["created_timestamp", "user", "product__name", ]
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return "Anonymous user"
+
+    def product_display(self, obj):
+        return str(obj.product.name)
