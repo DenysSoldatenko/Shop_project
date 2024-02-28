83d6336c7e118f23c48facec2863cb6e9f92ab4d
DenysSoldatenko <soldatenko911@gmail.com>
2024-02-28 10:00:00 +0300

    Added the DeliveryAndPaymentView

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from core.views import IndexView, AboutView
+from core.views import IndexView, AboutView, DeliveryAndPaymentView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('about', AboutView.as_view(), name='about'),
+    path('delivery-and-payment', DeliveryAndPaymentView.as_view(), name='delivery_and_payment'),
 ]
--- core/views.py
+++ core/views.py
@@ -12,7 +12,7 @@ class IndexView(TemplateView):
 
 
 class AboutView(TemplateView):
-    template_name = 'core/about_us.html'
+    template_name = 'core/info_page.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -26,5 +26,31 @@ class AboutView(TemplateView):
             Whether you're hosting friends, enjoying a family movie night, or simply relaxing with a book, we offer high-quality, stylish, and affordable furniture to make your space truly your own.
 
             Passionate about design and functionality, Luxe Living is your go-to destination for bringing comfort, sophistication, and innovation into your home. Browse our curated collections and see why our customers trust us to make their homes a reflection of their style and personality!
+        """
+        return context
+
+class DeliveryAndPaymentView(TemplateView):
+    template_name = 'core/info_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delivery and Payment'
+        context['content'] = "Delivery and Payment Information"
+        context['text_on_page'] = """
+            Welcome to Luxe Living's Delivery and Payment Information! We are thrilled to provide you with a seamless and exciting shopping experience, ensuring your order gets to you quickly, safely, and with ease!
+
+            Exciting Delivery Options:
+            - Standard Delivery: Reliable and affordable. Your furniture will arrive in just a few days.
+            - Express Delivery: Need it faster? Opt for express delivery and get your order delivered within 2-3 days!
+            - Same-day Delivery: In select locations, we offer same-day delivery — perfect for last-minute purchases or urgent needs! Imagine receiving your dream furniture on the same day you place the order!
+
+            Flexible Payment Methods:
+            - Credit/Debit Cards: Pay with major credit or debit cards, securely processed with encryption technology.
+            - PayPal: A fast and easy option to complete your purchase with just a few clicks.
+            - Cash on Delivery: For your convenience, we offer cash on delivery in selected locations! Pay when your furniture arrives — no need to worry about upfront payments.
+
+            We aim to make your shopping experience effortless, so you can focus on what matters most: creating a home that truly reflects your style and personality.
+
+            Thank you for choosing Luxe Living, where quality meets comfort.
         """
         return context
