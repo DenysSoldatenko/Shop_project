d6d844b894709f7de9e72741a48c549d3dff2e88
DenysSoldatenko <soldatenko911@gmail.com>
2024-03-03 10:00:00 +0300

    Added the UserLoginView

--- app/settings.py
+++ app/settings.py
@@ -147,3 +147,4 @@ INTERNAL_IPS = [ "127.0.0.1", ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.User'
 LOGIN_URL = '/user/login/'
+LOGIN_REDIRECT_URL = '/'
--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from user import views
-from user.views import UserCartView
+from user.views import UserCartView, UserLoginView
 
 app_name = 'user'
 
 urlpatterns = [
     path('cart/', UserCartView.as_view(), name='cart'),
     path('profile/', views.profile, name='profile'),
-    path('login/', views.login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
 ]
